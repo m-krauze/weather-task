9de214825307d18ab815eb2e69a83b09f93fb872
mickrauz <mickrauz@cisco.com>
2022-02-15 10:32:57 +0100

    Added Portal component with test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+/**
+ * Adding portal container
+ */
+const appPortal = document.createElement('div');
+appPortal.id = 'portal';
+document.body.append(appPortal);
